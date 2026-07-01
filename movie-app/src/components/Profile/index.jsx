13eee46b30ac1f7c64809a349f577d382e573fd0
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-01 19:38:29 +0530

    profile section  style changes

--- movie-app/src/components/Profile/index.jsx
+++ movie-app/src/components/Profile/index.jsx
@@ -1,9 +1,12 @@
 const Profile = () => {
     return (
         <>
-            <h1 className="font-bold text-2xl text-amber-500">
-                Profile Section
-            </h1>
+            <div className="max-w-md rounded-2xl p-5 mx-auto my-3 bg-white">
+                <h1 className="font-bold text-2xl text-amber-500">
+                    Profile Section
+                </h1>
+            </div>
+
         </>
     )
 }
